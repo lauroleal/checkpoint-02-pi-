0d439a9706f3bb82b37baf7ffffde5e3982287d9
Lauro <lauroleal2011@gmail.com>
2021-12-06 01:46:51 -0300

    Entrega de CheckPoint II - Programação Imperativa

---
+++ curso.js
@@ -0,0 +1,150 @@
+
+ const aluno = require('./estudantes.js');
+ //const calcularMedia = require('./aluno.js');
+
+ 
+
+/*Em um arquivo diferente, crie o objeto literal curso que tem como atributos: nome do
+curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de
+estudantes (um array composto pelos alunos criados no passo 1).*/
+
+let curso ={
+    nomeDoCurso: 'Certified Tech Developer',
+    notaAprovacao: 7,
+    faltasMaximas: 7,
+    listaDeEstudantes: [aluno.listaAlunos],
+
+    /*Crie o método que permite adicionar alunos à lista do curso, ou seja, quando
+chamamos nosso método em nosso objeto curso, deverá adicionar um aluno a mais na
+propriedade lista de estudantes do objeto curso.*/
+
+    novoAluno: function(nomeAluno, faltasAluno, notasAluno){
+        let addNovoAluno = new aluno.Aluno(nomeAluno, faltasAluno, notasAluno);
+        aluno.listaAlunos.push(addNovoAluno);
+    },
+
+/*Crie um método para o objeto curso que receba um aluno (como parâmetro) e
+retorne true se ele aprovou no curso ou false em caso de reprovação. Para ser aprovado, o
+aluno tem que ter uma média igual ou acima da nota de aprovação e ter menos faltas que
+faltas máximas. Se tiver a mesma quantidade, tem que estar 10% acima da nota de
+aprovação.*/
+
+//aluno.Aluno.prototype.calcularMedia()
+
+
+
+aprovaAluno: function(nome){
+    curso.listaDeEstudantes.forEach(alunosDoArray => {
+        alunosDoArray.forEach(conteudoArray => {
+              if(conteudoArray.nomeAluno === nome){
+                 if(conteudoArray.faltasAluno < this.faltasMaximas && (conteudoArray.notasAluno.reduce((total, nota) => total + nota)/4) >= this.notaAprovacao){
+                   console.log(` O Aluno(a) ${nome} foi aprovado!`);
+                   console.log(true);
+                } else if (conteudoArray.faltasAluno >= this.faltasMaximas && (conteudoArray.notasAluno.reduce((total, nota) => total + nota)/4) >= ((this.notaAprovacao*10)/100 + this.notaAprovacao)) {
+                    console.log(`Aluno(a) ${nome} foi aprovado pela regra 10%`);
+                    console.log(true);
+                }else {
+                    console.log(` O Aluno(a) ${nome} foi Reprovado!`);
+                      console.log(false);
+                    }
+            }  
+    }) 
+  }) /*
+  if(!nome in curso.listaDeEstudantes){
+  console.log(`${nome} não é aluno(a) do curso`);
+}*/
+},
+
+/*Crie um método para o objeto curso que percorra a lista de estudantes e retorne um
+array de booleanos com os resultados se os alunos aprovaram ou não.*/
+
+alunoAprovados: function(){
+    let conteudoArray = [];
+    curso.listaDeEstudantes.forEach(alunosDoArray => {
+        alunosDoArray.forEach((conteudo, i) => {
+            conteudoArray [i] = conteudo;
+        })
+    })
+    let arrayAprova = [];
+    let verMelhor = [];
+    for(let cont = 0;cont < aluno.listaAlunos.length; cont++){  
+     if(conteudoArray[cont].faltasAluno < this.faltasMaximas && (conteudoArray[cont].notasAluno.reduce((total, nota) => total + nota)/4) >= this.notaAprovacao){
+        arrayAprova [cont] = true;
+        verMelhor [cont] =`${conteudoArray[cont].nomeAluno} foi aprovado`;
+     } else if (conteudoArray[cont].faltasAluno >= this.faltasMaximas && (conteudoArray[cont].notasAluno.reduce((total, nota) => total + nota)/4) >= ((this.notaAprovacao*10)/100 + this.notaAprovacao)) {
+        arrayAprova [cont] = true;
+        verMelhor [cont] =`${conteudoArray[cont].nomeAluno} foi aprovado`;
+     }else {
+        arrayAprova [cont] = false;
+        verMelhor [cont] =`${conteudoArray[cont].nomeAluno} foi Reprovado`;
+     }
+   
+}
+console.log('');
+console.log('=================================================================================');
+console.log('|----------------------- Array de booleanos ------------------------------------|');
+console.log('=================================================================================');
+console.log('');
+console.log(arrayAprova);
+console.log('');
+console.log('=================================================================================');
+console.log('|------------- Mostrando os alunos aprovados/reprovados ------------------------|');
+console.log('=================================================================================');
+console.log('');
+console.log(verMelhor);
+
+}
+
+} 
+    console.log('📌:::::::: Micro desafio - Passo 3 e 4 ::::::::::::::::::::::::::::::::::::::::::');
+    console.log('');
+    console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+    console.log('||||||||||||||||||||||- ESTAMOS NO ARQUIVO curso.js -||||||||||||||||||||||||||||');
+    console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+    console.log('|----------------------- Cadastrando Novos Alunos ------------------------------|');
+    console.log('|--------------------------- (4 Novos Alunos)-----------------------------------|');
+    console.log('=================================================================================');
+    console.log('');
+    curso.novoAluno('Bruno', 4, [10,9,5,6]);
+    curso.novoAluno('Kauana', 3, [10,9,5,8]);
+    curso.novoAluno('Gustavo', 5, [7,8,5,9]);
+    curso.novoAluno('William', 2, [10,4,5,6]);
+    curso.novoAluno('Rafael', 8, [10,2,8,6]);
+    console.log('');
+    console.log('=================================================================================');
+    console.log(`|---------------- Nosso "Banco" tem ${aluno.listaAlunos.length} alunos cadastrados até o momento --------|`);
+    console.log('|----------------------- Mostrando todos os Alunos Cadastrados -----------------|');
+    console.log('=================================================================================');
+    console.log('');
+    console.log(curso.listaDeEstudantes);
+
+    
+    console.log('');
+    console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+    console.log('📌:::::::::::::::::::::::::: Micro desafio - Passo 5 ::::::::::::::::::::::::::::');
+    console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+    console.log('');
+    console.log('=================================================================================');
+    console.log('|----------------------- Aluno Aprovado? ---------------------------------------|');
+    console.log('|--------------------------(Teste com 4 Alunos)---------------------------------|');
+    console.log('|-------------(Se aluno não existir não retornar nada)--------------------------|');
+    console.log('=================================================================================');
+    console.log('');
+
+ curso.aprovaAluno('Lauro');
+ curso.aprovaAluno('Bruno');
+ curso.aprovaAluno('Rafael');
+ curso.aprovaAluno('William');
+
+ console.log('');
+ console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+ console.log('📌:::::::::::::::::::::: Micro desafio - Passo 6 ::::::::::::::::::::::::::::::::');
+ console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('');
+ curso.alunoAprovados();
+
+ // pegando o tamnho da lista de estudantes
+ let tamArray = aluno.listaAlunos.length;
+
+ // Exportando o objeto curso e a variavel que recebeu o tamnho da lista de estudantes
+ module.exports = {curso,tamArray};
---
+++ estudantes.js
@@ -0,0 +1,194 @@
+
+
+/*Crie um objeto aluno que tenha como atributos: nome (string), quantidade de faltas
+(number) e notas (array de números). Crie um construtor para ele e importe-o como o
+módulo aluno.*/
+
+
+class Aluno {
+    constructor(nomeAluno, faltasAluno, notasAluno) {
+
+        this.nomeAluno = nomeAluno;
+        this.faltasAluno = faltasAluno;
+        this.notasAluno = notasAluno;
+    }
+
+    adcionarAluno() {
+        return {
+        nomeAluno: this.nomeAluno,
+        faltasAluno: this.faltasAluno,
+        notasAluno : this.notasAluno
+        }
+    }
+
+    /*Nosso objeto aluno terá o método calcularMedia que retorna a média de suas notas*/
+
+    calcularMedia() {
+        const media = this.notasAluno.reduce((total, nota) => total + nota);
+            return `A média das notas do aluno(a) ${this.nomeAluno} é ${media/4}!`;
+    }
+
+     /*Também terá um método chamado faltas, que simplesmente aumenta o número 
+     de faltas em 1.*/
+
+    faltas(){
+        
+            const falta = this.faltasAluno = this.faltasAluno + 1;
+            return `O aluno(a) ${this.nomeAluno} agora tem ${falta} faltas!`;        
+    }
+}
+
+console.log('');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('📌:::::::::::::::::::: Micro desafio - Passo 1 :::::::::::::::::::::::::::::::::');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('');
+console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log('||||||||||||||||||- ESTAMOS NO ARQUIVO estudante.js -||||||||||||||||||||||||||||');
+console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log('|--------------------- Foram adcionados 7 alunos -------------------------------|');
+console.log('');
+
+const aluno1 = new Aluno('Lauro', 2, [10,8,9,6]);
+aluno1.adcionarAluno()
+const aluno2 = new Aluno('Michele', 5, [8,9,10,7]);
+aluno2.adcionarAluno()
+const aluno3 = new Aluno('Igor', 6, [10,5,7,9]);
+aluno3.adcionarAluno()
+const aluno4 = new Aluno('Rodrigo', 3, [10,7,10,5]);
+aluno4.adcionarAluno()
+const aluno5 = new Aluno('Emanuel', 2, [9,8,10,5]);
+aluno5.adcionarAluno()
+const aluno6 = new Aluno('Camila', 0, [10,8,10,8]);
+aluno6.adcionarAluno()
+const aluno7 = new Aluno('Isabela', 1, [10,9,8,6]);
+aluno7.adcionarAluno()
+
+let listaAlunos = [];
+listaAlunos.push(aluno1, aluno2,aluno3, aluno4,aluno5, aluno6,aluno7);
+console.log('=================================================================================');
+console.log(`|---------------- Nosso "Banco" tem ${listaAlunos.length} alunos cadastrados até o momento ---------|`);
+console.log('|----------------------- Mostrando os Alunos Cadastrados -----------------------|');
+console.log('=================================================================================');
+console.log('');
+console.log(listaAlunos);
+
+console.log('');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('📌:::::::::::::::::::::::: Micro desafio - Passo 2 ::::::::::::::::::::::::::::::');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('');
+console.log('================================================================================');
+console.log('|----------------- Método Calcular Média ( teste com 3 alunos) ----------------|');
+console.log('================================================================================');
+console.log('');
+console.log(listaAlunos[3].calcularMedia());
+console.log(listaAlunos[2].calcularMedia());
+console.log(listaAlunos[4].calcularMedia());
+console.log('');
+console.log('================================================================================');
+console.log('|----------------- Método Adcionar Falta ( teste com 2 alunos)-----------------|');
+console.log('|------------------------ adcionando 3 faltas para cada -----------------------|');
+console.log('================================================================================');
+console.log('');
+console.log(listaAlunos[0].faltas());
+console.log(listaAlunos[0].faltas());
+console.log(listaAlunos[0].faltas());
+console.log(listaAlunos[2].faltas());
+console.log(listaAlunos[2].faltas());
+console.log(listaAlunos[2].faltas());
+console.log('');
+
+
+/*Crie um construtor para ele e importe-o como o
+módulo aluno.*/
+
+module.exports = {listaAlunos,Aluno};
+const lista = require('./curso.js');
+
+//exports.calcularMedia() = calcularMedia;
+
+
+/*Micro desafio - Passo 7
+
+Importe o módulo estudantes.js, que contém uma lista de alunos, dentro do arquivo
+que contém o objeto curso. 
+
+Substitua o conteúdo da propriedade lista de estudantes,
+
+pela lista de estudantes do arquivo estudantes.js e garanta que sigam funcionando todos os
+métodos corretamente. (revisar que o arquivo tenha todos os métodos corretamente).*/
+
+console.log('');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('📌:::::::::::::::::::::::: Micro desafio - Passo 7 ::::::::::::::::::::::::::::::');
+console.log(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
+console.log('');
+console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log('||||||||||||||||||||- VOLTAMOS PRO ARQUIVO estudante.js -||||||||||||||||||||||||');
+console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+console.log('');
+
+
+console.log('================================================================================');
+console.log('------- Armazenando os itens da lista de estudantes num array temporario -------');
+console.log('================================================================================');
+
+// armazenando os itens da lista de estudantes num array temporario
+let temp = [];
+lista.curso.listaDeEstudantes.forEach(alunosDoArray => {
+    alunosDoArray.forEach(conteudoArray => {
+    temp.push(conteudoArray);
+    })})
+
+console.log('================================================================================');
+console.log('----------------- Removendo os itens da lista de estudantes --------------------');
+console.log('================================================================================');
+
+// Removendo os itens da lista de estudantes
+for(let i=0;i <lista.tamArray;i++){
+    listaAlunos.pop(i);
+ }
+
+ console.log(' ⬇️ Resultado ⬇️')
+ console.log()
+ console.log(listaAlunos)
+ console.log()
+ console.log(' ⬆️ Resultado ⬆️ ')
+
+ console.log('================================================================================');
+ console.log('|--------------- Preenchendo o array com o conteudo do array temp -------------|');
+ console.log('================================================================================');
+
+ // colocando os itens do Array temporario na lista de estudantes
+ for(let i=0;i <lista.tamArray;i++){
+    listaAlunos[i] = temp[i];
+   
+ }
+ console.log(listaAlunos)
+
+ console.log('================================================================================');
+ console.log('|-------------------------- Testando os métodos -------------------------------|');
+ console.log('================================================================================');
+
+console.log('');
+console.log('================================================================================');
+console.log('|----------------- Método Calcular Média ( teste com 3 alunos) ----------------|');
+console.log('================================================================================');
+console.log('');
+console.log(listaAlunos[1].calcularMedia());
+console.log(listaAlunos[5].calcularMedia());
+console.log(listaAlunos[6].calcularMedia());
+console.log('');
+console.log('================================================================================');
+console.log('|----------------- Método Adcionar Falta ( teste com 4 alunos)-----------------|');
+console.log('|------------------------ adcionando 2 faltas para cada -----------------------|');
+console.log('================================================================================');
+console.log('');
+console.log(listaAlunos[0].faltas());
+console.log(listaAlunos[0].faltas());
+console.log(listaAlunos[2].faltas());
+console.log(listaAlunos[2].faltas());
+console.log(listaAlunos[5].faltas());
+console.log(listaAlunos[5].faltas());
+console.log('');
